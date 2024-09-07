0186732b63af72e19b132b014dea28c308c90e50
Ryan <ryan.buck@mymail.champlain.edu>
2024-09-07 17:35:31 -0400

    Edit script 4, update csv

--- week2/Process Management/Scripts/4.ps1
+++ week2/Process Management/Scripts/4.ps1
@@ -1,9 +1,7 @@
 ﻿$chrome = Get-Process | Where-Object { $_.ProcessName -eq "chrome" }
 
-if ($chrome.Length -eq $null) {
-    Start-Process "chrome.exe" "https://www.champlain.edu"
+if ($chrome.Length -eq 0) {
+    Start-Process chrome "https://www.champlain.edu"
 } else {
-    for ($i=0; $i -lt $chrome.Length; $i++) {
-        $chrome[$i].Kill()
-    }
+    Stop-Process -Name chrome
 }
