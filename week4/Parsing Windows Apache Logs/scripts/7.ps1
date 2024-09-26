ba09d349241a0b1d8a5efe9a25ad84fb8f7c7838
Ryan <ryan.buck@mymail.champlain.edu>
2024-09-26 15:08:18 -0400

    Add scripts for parsing windows apache logs

---
+++ week4/Parsing Windows Apache Logs/scripts/7.ps1
@@ -0,0 +1,2 @@
+﻿$A = Get-ChildItem -Path C:\xampp\apache\logs\*.log | Select-String -Pattern 'error'
+$A[-5..-1]
---
+++ week4/Parsing Windows Apache Logs/scripts/8.ps1
@@ -0,0 +1,13 @@
+﻿$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String -Pattern ' 404 '
+
+$regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+$ipsUnorganized = $regex.Matches($notfounds)
+
+$ips = @()
+for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+    $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+}
+
+# All my ip addresses start with 184
+$ips | Where-Object { $_.IP -ilike "184.*" }
