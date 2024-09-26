e5b4ad8ea0fee3f57b90716137683f5c1bccd312
Ryan <ryan.buck@mymail.champlain.edu>
2024-09-26 15:44:44 -0400

    Make apache function

---
+++ week4/Parsing Windows Apache Logs/scripts/Apache-Logs.ps1
@@ -0,0 +1,18 @@
+﻿function getIPs($page, $code, $browser) {
+    $page = '/' + $page + ' '
+    $code = ' ' + $code + ' '
+    $browser = ' ' + $browser + '/'
+    
+    $logs = Get-ChildItem -Path C:\xampp\apache\logs\*.log | Select-String -Pattern $page | Select-String -Pattern $code | Select-String -Pattern $browser
+    
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+    $ipsUnorganized = $regex.Matches($logs)
+
+    $ips = @()
+    for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+        $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+    }
+
+    return $ips
+}
---
+++ week4/Parsing Windows Apache Logs/scripts/main.ps1
@@ -0,0 +1,8 @@
+﻿﻿. (Join-Path $PSScriptRoot "Apache-Logs.ps1")
+clear
+
+$loggedIps = getIPs -page "*" -code "404" -browser "Firefox"
+
+# All my ip addresses start with 184
+$counts = $loggedIps | Group-Object IP
+$counts | Select-Object Count, Name
