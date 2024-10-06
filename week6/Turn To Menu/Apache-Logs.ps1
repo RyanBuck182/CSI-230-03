b800cc6d1b03621257f572890acf7c63483a33d4
ryanbuck182@ <ryanbuck182@gmail.com>
2024-10-06 15:31:26 -0400

    Add scripts for Turn To Menu

---
+++ week6/Turn To Menu/Apache-Logs.ps1
@@ -0,0 +1,41 @@
+﻿function getIPs($page, $code, $browser) {
+    $page = '/' + $page + ' '
+    $code = ' ' + $code + ' '
+    $browser = ' ' + $browser + '/'
+    
+    $logs = Get-ChildItem -Path C:\xampp\apache\logs\*.log | Select-String -Pattern $page | Select-String -Pattern $code | Select-String -Pattern $browser
+    
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+    $ipsUnorganized = $regex.Matches($logs)
+
+    $ips = @()
+    for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+        $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value; }
+    }
+
+    return $ips
+}
+
+function ApacheLogs1() {
+    $logsnotformatted = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+    
+    for ($i=0; $i -lt $logsnotformatted.Count; $i++) {
+        $words = $logsnotformatted[$i].Split(" ")
+
+        $tableRecords += [pscustomobject]@{ `
+            "IP" = $words[0]; `
+            "Time" = $words[3].Trim('['); `
+            "Method" = $words[5].Trim('"'); `
+            "Page" = $words[6]; `
+            "Protocol" = $words[7].Trim('"'); `
+            "Response" = $words[8]; `
+            "Referrer" = $words[10].Trim('"'); `
+            "Client" = $words[11..($words.Length-1)]; `
+        }
+    }
+
+    # Most of my ips start with 184
+    return $tableRecords | Where-Object { $_.IP -ilike "184.*" }
+}
---
+++ week6/Turn To Menu/main.ps1
@@ -0,0 +1,62 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Process-Management-4.ps1)
+
+clear
+
+$Prompt  = "Please choose your operation:`n"
+$Prompt += "1 - Display the last 10 apache logs`n"
+$Prompt += "2 - Display the last 10 failed logins`n"
+$Prompt += "3 - Display at risk users`n"
+$Prompt += "4 - Start chrome, or stop it if already running`n"
+$Prompt += "5 - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 5){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $logs = ApacheLogs1
+        Write-Host ($logs | Select -First 10 | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 2){ 
+        $userLogins = getFailedLogins 999
+        Write-Host ($userLogins | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 3) {
+        $timeSince = Read-Host -Prompt "Please enter the number of days to search back."
+        $userLogins = getFailedLogins $timeSince
+        
+        Write-Host ($userLogins | Group-Object -Property User | Where-Object { $_.Count -gt 10 } | Select-Object Name, Count | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 4){
+        $chrome = Get-Process | Where-Object { $_.ProcessName -eq "chrome" }
+
+        if ($chrome.Length -eq 0) {
+            Start-Process chrome "https://www.champlain.edu"
+        } else {
+            Stop-Process -Name chrome
+        }
+    }
+
+    else {
+        Write-Host "Please enter a number between 1 and 5."
+    }
+}
